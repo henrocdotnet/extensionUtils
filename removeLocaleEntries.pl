eb748a2c76fc282500607c54645f76d66f2b0df2
Jonah Bishop <git@borngeek.com>
2014-04-28 10:38:56 -0400

    Substantial cleanup for removeLocaleEntities.pl

--- removeLocaleEntries.pl
+++ removeLocaleEntries.pl
@@ -2,33 +2,74 @@
 use strict;
 use warnings;
 
+our $VERSION = "1.00";
+
 use Cwd;
 use Encode;
 use File::Find;
+use Getopt::Long;
 
 my $cwd = getcwd();
+my $inputFile = '';
+my $prefix = '';
 my $tempFile = 'cleaned_locale.dtd';
+my @toRemove = ();
 
-my @toRemove = (
-	"gblite.confirm.title",
-	"gblite.confirm.label.usure",
-	"gblite.confirm.label.yes",
-	"gblite.confirm.ak.yes",
-	"gblite.confirm.label.no",
-	"gblite.confirm.ak.no"
-);
+GetOptions("input=s" => \$inputFile,
+		   "prefix=s" => \$prefix,
+		   "help" => sub { usage(); exit 0; });
 
-print "Scanning locale folders...\n\n";
+print "\nRemove Locale Entries v$VERSION\n\n";
+print "Script Parameters\n";
+print " - Input file:   $inputFile\n";
+print " - Prefix:       $prefix\n\n";
+
+# Handle any entities on the command line
+foreach (@ARGV)
+{
+	push @toRemove, "${prefix}$_";
+}
+
+# Handle any entities in an input file
+if ($inputFile)
+{
+	if (! -f $inputFile)
+	{
+		print "ERROR: Unable to locate $inputFile!\n";
+		exit 1;
+	}
+	else
+	{
+		open my $in, '<', $inputFile or die "Cannot open $inputFile: $!";
+		while (<$in>)
+		{
+			chomp;
+			s/^\s*//;
+			s/\s*$//;
+			push @toRemove, "${prefix}$_";
+		}
+		close $in;
+	}
+}
+
+print "Entities to be removed:\n";
+foreach (@toRemove)
+{
+	print " - $_\n";
+}
+
+print "\nScanning locale folders...\n\n";
 find(\&wanted, $cwd);
 
 sub wanted
 {
+	return if $File::Find::name !~  m!chrome/locale!;
 	return if ! m/\.dtd$/;
 	
 	my $id = $File::Find::dir;
 	$id =~ s!^.+/(.+)$!$1!;
 	
-	printf("%5s", $id);
+	printf("%7s", $id);
 	
 	my @dtds = <*.dtd>;
 	foreach my $f (@dtds)
@@ -44,7 +85,7 @@ sub wanted
 			
 			foreach my $match (@toRemove)
 			{
-				if(m/$match/)
+				if(m/\Q$match\E/)
 				{
 					$removed++;
 					$skip = 1;
@@ -70,4 +111,38 @@ sub wanted
 			print "  --  Nothing to do!\n";
 		}
 	}
+}
+
+sub usage
+{
+print <<USAGE;
+Script Usage:
+  removeLocaleEntries.pl [Options] [Entities]
+  
+Description:
+  This script is used to remove specified entities from all locales in a Firefox
+  extension's locale folder structure, making it very easy to remove deprecated
+  strings from a project.
+  
+[Entities]
+  A space separated list of entity IDs to be removed from the various locale
+  files.
+  
+[Options]
+  --prefix SOME_STRING
+  If specified, prepends SOME_STRING to each entity ID that needs to be removed,
+  saving you from having to type the same prefix a number of times.
+  
+  --input FILENAME
+  Specifies the input filename from which to read entity IDs to remove
+  
+Example:
+  removeLocaleEntries.pl --prefix gblite.confirm. title label.yes ak.yes
+  
+  The above example will remove the following entities from each DTD file found
+  in the project:
+    * <!ENTITY gblite.confirm.title "...">
+    * <!ENTITY gblite.confirm.label.yes "...">
+    * <!ENTITY gblite.confirm.ak.yes "...">
+USAGE
 }
