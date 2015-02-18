8632e7ec1b6c3e707abb9b7be9a81a1983f47d91
Jonah Bishop <git@borngeek.com>
2015-02-18 13:09:56 -0500

    Add functionality to compareLocales.pl
    The compareLocales.pl script needed a few improvements. First, the
    script wasn't smart enough to locate the locale folder on its own;
    this has now been fixed. Second, missing entities weren't reported
    on, skewing data ever so slightly. I've added tracking information
    for missing entities, printing out that data in the final summary.

--- compareLocales.pl
+++ compareLocales.pl
@@ -2,7 +2,7 @@
 use strict;
 use warnings;
 
-our $VERSION = "1.00";
+our $VERSION = "1.01";
 
 use Cwd;
 use Encode;
@@ -26,16 +26,31 @@ else
 	chdir $target or die "Unable to change to $target: $!";
 }
 
+if (! -d $masterLocale)
+{
+	# We didn't find the master locale, so look for a chrome/locale folder
+	if(-d "$target/chrome/locale")
+	{
+		$target .= "/chrome/locale";
+		chdir $target or die "Unable to change to $target: $!";
+	}
+	elsif(-d "$target/locale")
+	{
+		$target .= "/locale";
+		chdir $target or die "Unable to change to $target: $!";
+	}
+	else
+	{
+		print "ERROR: Unable to locate master locale folder ($masterLocale): $!";
+		exit 1;
+	}
+}
+
 print "\nLocale Comparison v$VERSION\n\n";
 print "Parameters:\n";
 print " - Master locale:    $masterLocale\n";
+print " - Verbose output:   $verbose\n";
 print " - Target directory: $target\n";
-
-if (! -d $masterLocale)
-{
-	print "ERROR: Unable to locate master locale folder ($masterLocale): $!";
-	exit 1;
-}
 
 chdir $masterLocale;
 
@@ -50,11 +65,15 @@ foreach my $file (@masterFiles)
 	open my $in, '<', $file;
 	while (<$in>)
 	{
-		my ($id, $value) = (m/<!ENTITY ([^ \t]+) "([^"]+)">/);
-		if (defined $id && defined $value)
+		if(m/<!ENTITY ([^ \t]+) "([^"]+)">/)
 		{
-			$masterEntities{$id} = $value;
+			$masterEntities{$1} = $2;
 		}
+		# my ($id, $value) = (m/<!ENTITY ([^ \t]+) "([^"]+)">/);
+		# if (defined $id && defined $value)
+		# {
+		# 	$masterEntities{$id} = $value;
+		# }
 	}
 	close $in;
 	print "(" . scalar(keys %masterEntities) . " entries)\n";
@@ -67,15 +86,17 @@ binmode STDOUT, ':utf8';
 print "\nScanning other locales...\n";
 find(\&wanted, $target);
 
-print "\nLocale   Matches   % Match\n";
-print "--------------------------\n";
-printf("%6s   %7d   {Master}\n\n", $masterLocale, scalar(keys %masterEntities));
+print "\nLocale   Matches   % Match   Missing   % Missing\n";
+print "------------------------------------------------\n";
+printf("%6s   %7d   {Master Locale}\n\n", $masterLocale, scalar(keys %masterEntities));
 
 foreach my $key (sort keys %stats)
 {
 	my $shortKey = $key;
 	$shortKey =~ s!^.+/(.+)$!$1!;
-	printf("%6s   %7d   %6.1f%%\n", $shortKey, $stats{$key}, ($stats{$key} / scalar(keys %masterEntities)) * 100);
+	printf("%6s   %7d   %6.1f%%   %7d   %8.1f%%\n", $shortKey, $stats{$key}{'matches'},
+	       ($stats{$key}{'matches'} / scalar(keys %masterEntities)) * 100,
+	       $stats{$key}{'missing'}, ($stats{$key}{'missing'} / scalar(keys %masterEntities)) * 100);
 }
 
 exit 0;
@@ -91,23 +112,36 @@ sub wanted
 	my @dtds = <*.dtd>;
 	foreach my $f (@dtds)
 	{
+		my %locale = ();
 		my $matches = 0;
+		my $missing = 0;
 		
 		open my $in, '<:encoding(UTF-8)', $f or die "Cannot open $f: $!";
 		while (<$in>)
 		{
 			next if m/^\s*$/; # Skip blank lines
-			
+
 			my ($id, $value) = m/<!ENTITY ([^ \t]+) "([^"]+)">/;
+			$locale{$id} = $value;
 			if ($masterEntities{$id} eq $value && length $value > 1)
 			{
-				print " - Duplicate ($id)\n" if $verbose;
+				print " - Duplicate ($id - \"$masterEntities{$id}\")\n" if $verbose;
 				$matches++;
 			}
 		}
 		close $in;
+
+		foreach (sort keys %masterEntities)
+		{
+			if(! exists $locale{$_})
+			{
+				print " - Missing ($_ - \"$masterEntities{$_}\")\n" if $verbose;
+				$missing++;
+			}
+		}
 		
-		$stats{$File::Find::dir} = $matches;
+		$stats{$File::Find::dir}{'matches'} = $matches;
+		$stats{$File::Find::dir}{'missing'} = $missing;
 	}
 }
 
